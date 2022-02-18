b8ce1953a3342ce75827e845bad91c984db57f17
paula-m <61393624+paula-m@users.noreply.github.com>
2022-02-18 17:43:41 +0000

    Add files via upload

---
+++ MonteCarloupdate.py
@@ -0,0 +1,162 @@
+from ensurepip import bootstrap
+from MonteCarlo import Dynamics
+
+import random
+import numpy as np
+import matplotlib
+import matplotlib.pyplot as plt
+import matplotlib.animation as animation
+matplotlib.use('TKAgg')
+import csv
+
+
+class MonteCarlo():
+    """The Class which goes through the metropolis algorithm, visualisation and calculation of the errors"""
+    def __init__(self):
+        self.N = int(input("size "))
+        self.dynamics = Dynamics(self.N) #initialises the class for the dynamics
+    
+    def creating_array(self):
+        """The method which creates random array for the random start."""
+        spin_lattice = np.zeros((self.N,self.N))    #creates numpy array of zeros
+        for i in range(self.N):
+            for j in range(self.N):
+                spin_lattice[i,j] = random.choice([-1,1])   #replacing each number with random number 1 or -1
+        return spin_lattice
+    
+    
+    def animation(self, spin_lattice):
+        f = open('spins.data', 'w') #opens the data for animation
+        for k in range(len(spin_lattice[0])):
+            for l in range(len(spin_lattice)):
+                f.write('%d %d %lf \n'%(k,l, spin_lattice[k,l]))    #writesthe spin and coordinates
+        f.close()   #closes file
+        plt.cla()   #clears the figure plot to plot the next animation
+        im=plt.imshow(spin_lattice, animated=True, cmap='Pastel1')  #animates the spin lattice
+        plt.draw()
+        plt.pause(0.001)
+        
+    def monte_carlo_update_kawasaki(self, nosteps, m, spin_lattice, T):
+        """The monte carlo kawasaki dynamics - we are not looking at magnetisation and susceptibility because it stays the same."""
+        
+        enery_arr = []
+        
+        for i in range(nosteps):    #the sweep (MC)
+            spin_lattice = self.dynamics.kawasaki_dynamics(len(spin_lattice), m, spin_lattice, T)   #calling the kawasaki dynamics
+            
+            #self.animation(spin_lattice)    #calls the animation function comment out if animation not needed
+        
+            if i >= 200:    #looking at the metropolis sweeps after they already have been thermalised
+                if (i%10) == 0: #only want to get measurements when they are independent each 10 sweeps
+                    energy = 0  #calculates energy
+                    for p in range(len(spin_lattice[0])):
+                        for n in range(len(spin_lattice)):
+                            energy += self.dynamics.energy_from_spin(spin_lattice, p, n)    #this calculates energy from each
+                    enery_arr.append((energy/2))    #to account for overcounting and boundary conditions.
+
+        scaled_heat_cap = (1/(m*(T**2)))*((np.mean(np.array(enery_arr)**2))-((np.mean(enery_arr))**2))
+
+        bootstrap_heat = self.bootstrap_specific_heat(enery_arr, T) #calls the function to get the error on the heat capacity for this temp
+        
+        k = open('data_kawasaki.csv', 'a+')  #saves the data from kawasaki for a csv file for calculations later on.
+        writer = csv.writer(k)
+        writer.writerow([T, np.mean(enery_arr), scaled_heat_cap, bootstrap_heat])
+        k.close()
+
+        return spin_lattice
+        
+        
+    def monte_carlo_update_glauber(self, nosteps, m, spin_lattice, T):
+        """method for the glauber update same type as kawasaki but has the magnetisation"""
+        magnetisation_array = []
+        enery_arr = []
+
+        for i in range(nosteps):
+            spin_lattice = self.dynamics.glauber_update(m, spin_lattice, T)
+            self.animation(spin_lattice)
+            
+            if i >= 200:
+                if (i%10) == 0:
+                    a = np.sum(spin_lattice)
+                    magnetisation_array.append(a)
+                    energy = 0
+                    for p in range(len(spin_lattice[0])):
+                        for n in range(len(spin_lattice)):
+                            energy += self.dynamics.energy_from_spin(spin_lattice, p, n)
+                    enery_arr.append((energy/2))
+        plt.close()
+        susceptibility = (1/(m*T))*((np.mean(np.array(magnetisation_array)**2)) - (np.mean(magnetisation_array)**2))
+        scaled_heat_cap = (1/(m*(T**2)))*((np.mean(np.array(enery_arr)**2))- (np.mean(enery_arr)**2))
+
+        bootstrap_err_heat = self.bootstrap_specific_heat(enery_arr, T, m)
+        bootstrap_susc = self.bootstrap_susceptibility(magnetisation_array, T, m)
+        
+        k = open('data_glauber_t1.csv', 'a')
+        writer = csv.writer(k)
+        writer.writerow([T, np.mean(magnetisation_array), np.mean(enery_arr), susceptibility, scaled_heat_cap, bootstrap_err_heat, bootstrap_susc])
+        k.close()
+        return spin_lattice    
+
+
+    def choice_of_dynamics(self, nosteps, spin_lattice):
+        """The method class for the monte carlo update asking for input"""
+        dynamics_choice = input("Please give dynamics, kawasaki or glauber: ")
+        T = int(input("Temperature "))
+        latticesize = (self.N)**2
+        if dynamics_choice == "kawasaki":
+            self.monte_carlo_update_kawasaki(nosteps, latticesize, spin_lattice, T)
+        elif dynamics_choice == "glauber":
+            self.monte_carlo_update_glauber(nosteps, latticesize, spin_lattice, T) 
+        else:
+            print("Please type correct dynamics")   #if typo then try again
+            self.choice_of_dynamics(nosteps, latticesize, spin_lattice, T)
+      
+    
+    def bootstrap_specific_heat(self, lattice, T, m):
+        """Errors for calculating specific heat """
+        all_c = []
+        for j in range(1000):
+            new_sample = []
+            for i in range(len(lattice)):
+                ind = random.randrange(len(lattice))    #selects index of random site
+                new_sample.append(lattice[ind]) #appends the selected energy into array
+            spec_heat = ((np.mean(np.array(new_sample)**2)) - (np.mean(np.array(new_sample)))**2)*(1/((m)*(T**2)))   #calculates SHC for the new array of energies 
+            all_c.append(spec_heat)
+        error = np.sqrt((np.mean(np.array(all_c)**2)) - (np.mean(np.array(all_c))**2))  #calculates the error.
+        return error
+    
+    def bootstrap_susceptibility(self, lattice, T, m):
+        """Errors for calculating susceptibility"""
+        all_c = []
+        for j in range(1000):
+            new_sample = []
+            for i in range(len(lattice)):
+                ind = random.randrange(len(lattice))
+                new_sample.append(lattice[ind])
+            spec_heat = ((np.mean(np.array(new_sample)**2)) - (np.mean(np.array(new_sample)))**2)*(1/((m)*T))
+            all_c.append(spec_heat)
+        error = np.sqrt((np.mean(np.array(all_c)**2)) - (np.mean(np.array(all_c))**2))
+        return error
+            
+      
+
+def main():
+    try_dyn = MonteCarlo()
+    
+
+    spin_lattice = try_dyn.creating_array()
+    try_dyn.choice_of_dynamics(2500, spin_lattice)
+    
+    #The code for long run for glauber 
+    #spin_lattice = (-1)*np.ones((50,50))
+    #temp_arr = np.arange(1, 3.1, 0.1)
+    #for j in range(len(temp_arr)):
+    #    spin_lattice = try_dyn.monte_carlo_update_glauber(10200, 2500, spin_lattice, temp_arr[j])
+    
+    #The code for long run for kawasaki
+    #spin_lattice = try_dyn.creating_array()
+    #for i in range(len(temp_arr)):
+    #    spin_lattice = try_dyn.monte_carlo_update_kawasaki(10200, 2500, spin_lattice, temp_arr[i])
+        
+
+main()
---
+++ plotting_graphs.py
@@ -0,0 +1,85 @@
+from turtle import color
+import numpy as np
+import csv
+import matplotlib
+import matplotlib.pyplot as plt
+import pandas as pd
+
+class plotting_graphs():
+
+    def plot_glauber(self):
+        column_names = ["Temperature", "Magnetisation", "Energy", "Susceptibility", "Specific", "Errorheat", "Errorsus"]
+        df = pd.read_csv("data_glauber_t1.csv", names = column_names)
+        Energy = df.Energy.to_list()
+        Temp = df.Temperature.to_list()
+        Magnetisation = df.Magnetisation.to_list()
+        Magnetisation_1 = list(-(np.array(Magnetisation[0:21])))
+        Suscept = df.Susceptibility.to_list()
+        Heat_cap = df.Specific.to_list()
+        Heaterr = df.Errorheat.to_list()
+        Suserr = df.Errorsus.to_list()
+        
+        Suscept = list((np.array(Suscept))/50)
+
+        plt.plot(Temp[0:21], Energy[0:21], label="1st data")
+        #plt.scatter(Temp[0:21], Energy[21:42], color="purple", label="2nd data")
+        plt.title("The Energy vs Temp - Glauber")
+        plt.xlabel("Temperature")
+        plt.ylabel("Energy")
+        plt.legend()
+        plt.show()
+
+        plt.plot(Temp[0:21], Magnetisation[0:21], label="Normal magnetisation")
+        #plt.plot(Temp[0:21], Magnetisation[21:42], color="purple", label="Absolute Magnetisation")
+        plt.title("Magnetisation vs Temp - Glauber")
+        plt.xlabel("Temperature")
+        plt.ylabel("Magnetisation")
+        plt.legend()
+        plt.show()
+        
+        plt.plot(Temp[0:21], Suscept[0:21], color="orange", ms=5)
+        plt.errorbar(Temp[0:21], Suscept[0:21], yerr=Suserr[0:21], ecolor="purple", fmt="none")
+        #plt.errorbar(Temp[0:21], Suscept[21:42], yerr=Suserr[21:42], ecolor="purple")
+        plt.title("Susceptibility vs Temp - Glauber")
+        plt.xlabel("Temperature")
+        plt.ylabel("Susceptibility")
+        plt.show()
+        
+        plt.plot(Temp[0:21], Heat_cap[0:21])
+        #plt.plot(Temp[0:21], Heat_cap[21:42])
+        plt.errorbar(Temp[0:21], Heat_cap[0:21], yerr=Heaterr[0:21], ecolor="purple", fmt="none")
+        plt.title("Heat Capacity vs Temperature - Glauber")
+        plt.xlabel("Temperature")
+        plt.ylabel("Heat Capacity")
+        plt.show()
+        
+
+    def plot_kawasaki(self):
+        column_names = ["Temperature", "Energy", "Specific", "Error"]
+        df = pd.read_csv("data_kawasaki.csv", names=column_names)
+        #print(df)
+        
+        energy = df.Energy.to_list()
+        specific = df.Specific.to_list()
+        Temperature = df.Temperature.to_list()
+        Error = df.Error.to_list()
+        #print(energy)
+
+        plt.plot(Temperature, energy)
+        plt.title("the temperature vs energy - Kawasaki")
+        plt.xlabel("Temperature")
+        plt.ylabel("Energy")
+        plt.show()
+        
+        plt.plot(Temperature, specific)
+        plt.errorbar(Temperature, specific, yerr=Error)
+        plt.title("The Heat Capacity vs Temp - Kawasaki")
+        plt.xlabel("Temperature")
+        plt.ylabel("Heat Capacity")
+        plt.show()
+    
+def main():
+    a = plotting_graphs()
+    a.plot_kawasaki()
+    a.plot_glauber()
+main()
